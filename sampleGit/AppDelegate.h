ea330f8c8bf036a3b7b8c3686742c72ebab8667c
Gorav Grover <goravgrover@Goravs-MacBook-Pro.local>
2018-05-11 10:46:22 +0530

    Initial Commit

---
+++ sampleGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleGit
+//
+//  Created by Gorav Grover on 11/05/18.
+//  Copyright © 2018 Gorav Grover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleGit
+//
+//  Created by Gorav Grover on 11/05/18.
+//  Copyright © 2018 Gorav Grover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
